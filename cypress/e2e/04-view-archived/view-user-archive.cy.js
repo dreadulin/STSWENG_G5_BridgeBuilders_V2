055b8d89af7d9f52e3e8ab9c4159552b594452bb
jjg-git <john_joseph_giron@dlsu.edu.ph>
2024-11-18 20:00:35 +0800

    Added view-user-archive.cy.js

---
+++ cypress/e2e/04-view-archived/view-user-archive.cy.js
@@ -0,0 +1,33 @@
+describe("US# 4", () => {
+  beforeEach(() => {
+    cy.login("gege", "akutami");
+  })
+  it("Verify if the user can view children's archived files", () => {
+  });
+});
+
+/*
+describe(
+  "Verify if admin can view " + "both home care and children’s archived files",
+  () => {
+    it("passes", () => {
+      cy.visit("/");
+    });
+  }
+);
+
+describe("Verify if the user can view children's archieved files", () => {
+  it("passes", () => {
+    cy.visit("/");
+  });
+});
+
+describe(
+  "Verify if the staff cannot access archived files in the wrong category",
+  () => {
+    it("passes", () => {
+      cy.visit("/");
+    });
+  }
+);
+*/
