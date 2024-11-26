2725620d05dd4973a42f5ade1f5580ee4f25d352
jjg-git <john_joseph_giron@dlsu.edu.ph>
2024-11-26 16:40:14 +0800

    (test-builder) Added File class for attachedFiles of Child class

---
+++ test-builder/make_file.js
@@ -0,0 +1,9 @@
+export class File {
+  constructor() {
+    this.fileName = undefined;
+    this.fileType = undefined;
+    this.uploadDate = undefined;
+    this.fileId = undefined;
+    this.fileStatus = undefined;
+  }
+}
