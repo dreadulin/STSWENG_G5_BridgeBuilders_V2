34d0fa3140b8c90d4a492b32642ccbe667e3bf8e
jjg-git <john_joseph_giron@dlsu.edu.ph>
2024-11-26 19:30:13 +0800

    (test-builder) Added parameters into the constructor of File

--- test-builder/make_file.js
+++ test-builder/make_file.js
@@ -1,9 +1,22 @@
+export const ACTIVE = "Active";
+export const DELETED = "Deleted";
+
+/**
+ * File
+ */
 export class File {
-  constructor() {
-    this.fileName = undefined;
-    this.fileType = undefined;
-    this.uploadDate = Date.now();
-    this.fileId = undefined;
-    this.fileStatus = "Active";
+  /**
+   * Created a file
+   * @param {string} filename - Filename of the file 
+   * @param {string} fileType - type of file (e.g., "image/png", "application/pdf")
+   * @param {number} fileId - unique id
+   * @param {string} fileStatus - use the exported constants [ACTIVE, DELETED]
+   */
+  constructor(fileName, fileType, fileId, fileStatus) {
+    this.fileName = fileName;
+    this.fileType = fileType;
+    this.uploadDate = new Date();
+    this.fileId = fileId;
+    this.fileStatus = fileStatus;
   }
 }
